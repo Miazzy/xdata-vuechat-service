b6907552519bbdccf4b1ca0a871caca8746787d8
yunwisdom <qq695926920@163.com>
2020-07-08 11:30:23 +0800

    init

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -4,11 +4,11 @@
       <div class="weui-cells" style="margin-top:-15px;">
         <img src="http://weather.shengtai.club/static/media/background.c327aeb9.jpg" style="height:155px;width:100%;">
 
-        <router-link to="" class="weui-cell weui-cell_access" style="padding:3px 5px;padding-left:10px;border-top:0px solid #ffffff;">
+        <router-link to="" class="weui-cell weui-cell-app_access" style="padding:3px 5px;padding-left:10px;border-top:0px solid #ffffff;">
           <div class="weui-cell__hd" style="width:22px; border-radius: 4px; margin-top: 0px;">
             <img src="//cdn.jsdelivr.net/gh/Miazzy/yunwisdom_cdn@v1.0.0/images/announce.png" style="width:22px; border-radius: 4px; margin-top: 0px;">
           </div>
-          <div class="weui-cell__bd" style="font-size:13px;margin-top:-2px;margin-left:-1px;">
+          <div class="weui-cell__bd" style="text-align:left;font-size:13px;margin-top:-2px;margin-left:-1px;">
             公告
           </div>
         </router-link>
@@ -16,7 +16,7 @@
 
       <div class="weui-cells">
         <div class="weui-cell-title">常用应用</div>
-        <div class="weui-cell weui-cell_access" id="scanCell">
+        <div class="weui-cell weui-cell-app_access" id="scanCell">
           <div class="weui-cell_app_hd">
            <img src="//cdn.jsdelivr.net/gh/Miazzy/yunwisdom_cdn@v1.0.0/images/time_00.png" >
             <div class="weui-cell_app_bd" >
@@ -46,7 +46,7 @@
 
       <div class="weui-cells">
         <div class="weui-cell-title">人事管理</div>
-        <div class="weui-cell weui-cell_access" id="scanCell">
+        <div class="weui-cell weui-cell-app_access" id="scanCell">
           <div class="weui-cell_app_hd">
            <img src="https://cdn.jsdelivr.net/gh/Miazzy/yunwisdom_cdn@v1.0.0/images/qiandao.png" >
             <div class="weui-cell_app_bd" >
@@ -72,7 +72,7 @@
             </div>
           </div>
         </div>
-        <div class="weui-cell weui-cell_access" id="scanCell">
+        <div class="weui-cell weui-cell-app_access" id="scanCell">
           <div class="weui-cell_app_hd">
            <img src="//cdn.jsdelivr.net/gh/Miazzy/yunwisdom_cdn@v1.0.0/images/person.png" >
             <div class="weui-cell_app_bd" >
@@ -102,7 +102,7 @@
 
       <div class="weui-cells">
         <div class="weui-cell-title">财务管理</div>
-        <div class="weui-cell weui-cell_access" id="scanCell">
+        <div class="weui-cell weui-cell-app_access" id="scanCell">
           <div class="weui-cell_app_hd">
            <img src="https://cdn.jsdelivr.net/gh/Miazzy/yunwisdom_cdn@v1.0.0/images/qiandao.png" >
             <div class="weui-cell_app_bd" >
@@ -132,7 +132,7 @@
 
       <div class="weui-cells">
         <div class="weui-cell-title">差旅商旅</div>
-        <div class="weui-cell weui-cell_access" id="scanCell">
+        <div class="weui-cell weui-cell-app_access" id="scanCell">
           <div class="weui-cell_app_hd">
            <img src="//cdn.jsdelivr.net/gh/Miazzy/yunwisdom_cdn@v1.0.0/images/trip.png" >
             <div class="weui-cell_app_bd" >
