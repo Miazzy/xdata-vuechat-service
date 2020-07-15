a4a493c717ea23eedb92a07cd6aa5ca500d47005
yunwisdom <qq695926920@163.com>
2020-07-15 17:16:18 +0800

    init

--- src/request/rest.js
+++ src/request/rest.js
@@ -1,6 +1,7 @@
 import * as storage from '@/request/storage';
+import * as tools from '@/request/tools';
 
-var queryUserInfoByView = async(username) => {
+export async function queryUserInfoByView(username) {
     //更新URL PATCH	/api/tableName/:id	Updates row element by primary key http://api.shengtai.club/api/v_admin?_where=(username,eq,zhaoziyun)
     var queryURL = `${window.requestAPIConfig.restapi}/api/v_user?_where=(username,eq,${username})`;
 
@@ -21,7 +22,7 @@ var queryUserInfoByView = async(username) => {
             storage.setStore(
                 `system_v_user_info@username$${username}`,
                 result,
-                3600 * 2
+                3600 * 24
             );
         }
     } catch (err) {
@@ -30,4 +31,61 @@ var queryUserInfoByView = async(username) => {
 
     //返回查询后的动态数据
     return result;
+}
+
+/**
+ * @function 查询表字段信息
+ * @param {*} tableName
+ */
+export async function queryTableFieldInfoJSON(tableName) {
+    //初始化
+    window.tools = window.tools == null ? tools : window.tools;
+
+    try {
+        //查询表单信息
+        var tableInfo = await queryTableDataByField(
+            'v_table_info',
+            'id',
+            tableName
+        );
+        //如果信息不为空，则解析表单信息
+        if (window.tools.deNull(tableInfo) != '' && tableInfo.length > 0) {
+            try {
+                tableInfo = window.tools.deNull(tableInfo[0]['value']);
+            } catch (error) {
+                console.log('tabale info :' + tableInfo);
+            }
+        }
+        //如果信息不为空，则进行解析数据
+        if (window.tools.deNull(tableInfo) != '') {
+            try {
+                tableInfo = JSON.parse(tableInfo);
+            } catch (error) {
+                console.log('tabale info :' + tableInfo);
+            }
+        }
+    } catch (error) {
+        console.log('query table field info json error :' + error);
+    }
+    return tableInfo;
+}
+
+/**
+ * 查询数据
+ * @param {*} tableName
+ * @param {*} foreignKey
+ * @param {*} id
+ */
+export async function queryTableDataByField(tableName, field, value) {
+    //大写转小写
+    tableName = tableName.toLowerCase();
+    //更新URL PATCH	/api/tableName/:id	Updates row element by primary key
+    var queryURL = `${window.requestAPIConfig.restapi}/api/${tableName}?_where=(${field},eq,${value})`;
+
+    try {
+        var res = await superagent.get(queryURL).set('accept', 'json');
+        return res.body;
+    } catch (err) {
+        console.log(err);
+    }
 }
