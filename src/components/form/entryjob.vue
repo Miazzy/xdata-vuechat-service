ccc5f8f89c3679f7e49d39ceaf6062b7b5ecce82
yunwisdom <qq695926920@163.com>
2020-09-15 09:16:41 +0800

    init

--- src/components/form/entryjob.vue
+++ src/components/form/entryjob.vue
@@ -185,6 +185,10 @@
                 <van-field :readonly="readonly" required clearable label="身份证号" v-model="item.idcard" placeholder="请输入您的身份证编号！" @blur="validField('idcard');" :error-message="message.idcard" />
                 <!-- 银行卡号（1寸照片，用于制作工牌） -->
                 <van-field :readonly="readonly" required clearable label="银行卡号" v-model="item.bank_card" placeholder="请输入您的工资卡对应银行卡号！" @blur="validField('bank_card');" :error-message="message.bank_card" />
+              </van-cell-group>
+
+              <van-cell-group style="margin-top:10px;">
+                <van-cell value="学历信息" style="margin-left:0px;margin-left:-3px;font-size: 0.95rem;" />
                 <!-- 学历编号（HR需要确认/修改） -->
                 <van-field :readonly="readonly" v-show="item.greatdiploma == '专科' || item.greatdiploma == '本科' || item.greatdiploma == '硕士' ||  item.greatdiploma == '博士'" clearable label="毕业证号" v-model="item.diploma" placeholder="请输入您的毕业证书编号！" @blur="validField('diploma');" :error-message="message.diploma" />
                 <!-- 学位编号（1寸照片，用于制作工牌） -->
