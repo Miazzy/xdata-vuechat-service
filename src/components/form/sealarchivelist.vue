38d2763c5bcaeb15840db1ded4771e2a9e3baec9
yunwisdom <qq695926920@163.com>
2020-10-09 17:49:19 +0800

    init

--- src/components/form/sealarchivelist.vue
+++ src/components/form/sealarchivelist.vue
@@ -391,27 +391,27 @@ export default {
         if(this.tabname == '1'){
           storage.setStore('system_seal_archive_list_tabname' , this.tabname);
           //跳转到相应的用印界面
-          this.$router.push(`/app/sealview?id=${id}&statustype=none`);
+          this.$router.push(`/app/sealview?id=${id}&statustype=none&back=sealarchivelist`);
         } else if(this.tabname == '2' && item.seal_type == '非合同类'){
           storage.setStore('system_seal_archive_list_tabname' , this.tabname);
           //跳转到相应的用印界面
-          this.$router.push(`/app/sealreceive?id=${id}&statustype=none&type=receive`);
+          this.$router.push(`/app/sealreceive?id=${id}&statustype=none&type=receive&back=sealarchivelist`);
         } else if(this.tabname == '2' || this.tabname == '3'){
           storage.setStore('system_seal_archive_list_tabname' , this.tabname);
           //跳转到相应的用印界面
-          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=front`);
+          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=front&back=sealarchivelist`);
         } else if(this.tabname == '4' ){
           storage.setStore('system_seal_archive_list_tabname' , this.tabname);
           //跳转到相应的用印界面
-          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=done`);
+          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=done&back=sealarchivelist`);
         } else if(this.tabname == '5' ){
           storage.setStore('system_seal_archive_list_tabname' , this.tabname);
           //跳转到相应的用印界面
-          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=done`);
+          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=done&back=sealarchivelist`);
         } else if(this.tabname == '6' ){
           storage.setStore('system_seal_archive_list_tabname' , this.tabname);
           //跳转到相应的用印界面
-          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=done`);
+          this.$router.push(`/app/sealview?id=${id}&statustype=none&type=done&back=sealarchivelist`);
         }
 
       },
