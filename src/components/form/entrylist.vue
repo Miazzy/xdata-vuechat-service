eb4b3a602557dde606be11a6cb9abe35449dde1c
yunwisdom <qq695926920@163.com>
2020-10-12 20:35:26 +0800

    init

--- src/components/form/entrylist.vue
+++ src/components/form/entrylist.vue
@@ -11,6 +11,11 @@
                 <span>返回</span>
             </router-link>
             <span>入职进度</span>
+            <van-dropdown-menu id="header-drop-menu" class="header-drop-menu" @change="headDropMenu();" z-index="100" style="position: absolute; width: 45px; height: auto; right: -15px; top: -3px; opacity: 1; background:#1b1b1b; ">
+              <van-icon name="weapp-nav" size="1.3rem" @click="headMenuToggle" style="position: absolute; width: 40px; height: auto; right: 0px; top: 16px; opacity: 1; background:#1b1b1b;z-index:10000; " />
+              <van-icon name="search" size="1.3rem" @click="searchFlag = true;" style="position: absolute; width: 40px; height: auto; right: 42px; top: 17px; opacity: 1; background:#1b1b1b;z-index:10000;"  />
+              <van-dropdown-item v-model="dropMenuValue" ref="headMenuItem" :options="dropMenuOption" @change="headDropMenu();" />
+            </van-dropdown-menu>
         </div>
     </header>
 
@@ -74,6 +79,18 @@ export default {
               '2': 'confirmList',
               '3': 'doneList',
             },
+            searchFlag: false,
+            dropMenuOldValue:'',
+            dropMenuValue:'',
+            dropMenuOption: [
+              { text: '合同类', value: 0 , icon: 'records' },
+              { text: '非合同', value: 1 , icon: 'description' },
+              { text: '刷新', value: 2 , icon: 'replay' },
+              { text: '搜索', value: 3 , icon: 'search' },
+              { text: '重置', value: 4 , icon: 'aim' },
+              { text: '应用', value: 5 , icon: 'apps-o' },
+              { text: '首页', value: 6 , icon: 'wap-home-o' },
+            ],
             isLoading:false,
             loading:false,
             back:'/app',
