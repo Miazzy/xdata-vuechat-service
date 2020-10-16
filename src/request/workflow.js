39390d02bb5692922b1266ce504ae3b074394985
yunwisdom <qq695926920@163.com>
2020-10-16 14:50:05 +0800

    init

--- src/request/workflow.js
+++ src/request/workflow.js
@@ -25,7 +25,7 @@ export async function queryPRLogHistoryByDataID(business_data_id) {
         console.log(res);
 
         if (res.body != null && res.body.length > 0) {
-            storage.setStore(`sys_workflow_cache@$history&id${business_data_id}`, res.body, 3);
+            storage.setStore(`sys_workflow_cache@$history&id${business_data_id}`, res.body, 2);
         }
 
         return res.body;
