2b62212b9c7a876951d003e26cc1120413d522b0
yunwisdom <qq695926920@163.com>
2020-10-21 15:28:34 +0800

    init

--- src/request/manage.js
+++ src/request/manage.js
@@ -483,7 +483,7 @@ export async function queryContractInfoByPrefixAll(prefix) {
 
     try {
         //构建查询SQL
-        const sql = `${window.requestAPIConfig.restapi}/api/bs_seal_regist?_where=(contract_id,like,${prefix}~)~and(status,in,待用印,已退回,已废弃,已用印,已领取,移交前台,已完成,财务归档,档案归档,已归档)&_p=0&_size=10&_sort=-create_time`;
+        const sql = `${window.requestAPIConfig.restapi}/api/bs_seal_regist?_where=(contract_id,like,${prefix}~)~and(status,in,待用印,已退回,已废弃,已用印,已领取,移交前台,已完成,财务归档,档案归档,已归档)&_p=0&_size=3&_sort=-create_time`;
         //如果用印登记类型为合同类，则查询最大印章编号，然后按序使用更大的印章编号
         var maxinfo = await superagent.get(sql).set('accept', 'json');
 
