fa3f1bf47784cdd4c22ca359532afca55ea653a5
yunwisdom <qq695926920@163.com>
2020-11-18 15:34:09 +0800

    //如果前缀编号存在，则查询一下最近合同单号           if(that.item.prefix && that.item.status =='待用印'){             await this.queryHContract();           } 只有待用印的申请单，才自动推算合同编号

--- src/components/form/sealview.vue
+++ src/components/form/sealview.vue
@@ -66,7 +66,7 @@
               <van-field readonly clearable  label="审批类型" v-model="item.approveType" placeholder="选择审批类型" @click="tag.showPicker = true" />
               <van-field required clearable label="编号前缀" v-model="item.prefix" placeholder="请输入合同对应前缀，如LD、SD、CD等" v-show="item.sealtype == '合同类' || item.sealtype == '合同类' " @blur="validField('prefix');queryHContract();" :error-message="message.prefix" @click="queryHContract();"  />
               <van-field clearable label="合同编号" v-model="item.contractId" placeholder="请输入合同编号" v-show="item.sealtype == '合同类' " />
-              <van-address-list v-show="hContractList.length > 0 && item.sealtype == '合同类'" v-model="hContractID" :list="hContractList" default-tag-text="默认" edit-disabled @select="selectHContract()" />
+              <van-address-list v-show="hContractList.length > 0 && item.sealtype == '合同类' && item.status =='待用印' " v-model="hContractID" :list="hContractList" default-tag-text="默认" edit-disabled @select="selectHContract()" />
               <van-field :readonly="readonly" clearable label="签收人" v-model="item.signman" placeholder="请输入文件签收人" />
               <van-field required clearable label="用印公司" v-model="item.company" placeholder="请输入用印公司" />
               <van-field clearable label="流程编号" v-model="item.workno" placeholder="请输入流程编号" />
@@ -1018,7 +1018,7 @@ export default {
           }
 
           //如果前缀编号存在，则查询一下最近合同单号
-          if(that.item.prefix){
+          if(that.item.prefix && that.item.status =='待用印'){
             await this.queryHContract();
           }
 
