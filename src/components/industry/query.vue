c229950ab88751be85d50897000babefa55ab064
yunwisdom <qq695926920@163.com>
2021-01-20 11:30:38 +0800

    查询界面

--- src/components/industry/query.vue
+++ src/components/industry/query.vue
@@ -7,7 +7,7 @@
                     <img src="https://co-image.qichacha.com/CompanyImage/f50d972bf1d7aa5bb88da915da782762.jpg?x-oss-process=style/qcc_cmp" class="m-r-sm" alt="企业查询" />
                 </a>
             </div>
-            <ul>
+            <ul class="navi-nav pull-right lpan" style="position: relative">
                 <li class="dropdown user-drop" style="margin-right: 0px">
                     <a rel="nofollow" style="padding-left: 0px; padding-right: 15px" onclick="zhugeTrack('顶栏按钮点击',{'按钮名称':'个人中心'});" href="/user_home">
                         <span class="headface">
