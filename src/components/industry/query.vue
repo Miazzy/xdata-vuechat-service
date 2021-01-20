81072a2f463807b2d31d07af06fc2d69a3e3a0f7
yunwisdom <qq695926920@163.com>
2021-01-20 12:29:12 +0800

    查询界面

--- src/components/industry/query.vue
+++ src/components/industry/query.vue
@@ -35,33 +35,12 @@
                     </ul>
                     <form action="/web/search" class="search-form" id="indexSearchForm" style="positon:absolute;">
                         <div class="input-group"> 
-                          <input type="text" id="searchkey" maxlength="1000" autocomplete="off" placeholder="请输入企业名、人名、产品名、或地址电话/经营范围等，多关键词用空格隔开，如“苏州 企查查”" name="key" class="form-control index-searchkey" required="required"> <input type="hidden" id="index" name="index" value="0"> 
-                          <span class="input-group-btn" style="position:relative;top:-51px;left:86%;"> 
+                          <input style="width:100%;" type="text" id="searchkey" maxlength="1000" autocomplete="off" placeholder="请输入企业名、人名、产品名、或地址电话/经营范围等，多关键词用空格隔开，如“苏州 领地”" name="key" class="form-control index-searchkey" required="required"> <input type="hidden" id="index" name="index" value="0"> 
+                          <span class="input-group-btn" style="position:relative;top:0px;left:40px;"> 
                             <input type="submit" class="index-searchbtn" value="查一下" required="required"> 
                           </span> 
                         </div> 
                     </form>
-                    <section class="search-list" id="search-list" style="display: none;"> </section>
-                    <section class="oversea-drop" id="index-oversea-drop"></section>
-                    <div v-show="appflag" class="search-attach clearfix" >
-                        <div class="index-app-box" data-length="12">
-                            <div class="wrap-prev"></div>
-                            <div class="box-wrap">
-                                <div class="box-scroll clearfix" style="width: 1676px"> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'招标查查'});" target="_blank" href="https://t.qcc.com/home" class="index-icon-text">
-                                        <div class="nav-icon-hot"></div> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/36d06cbfe49a44edaea338c08660e5fa.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>招标查查</span>
-                                    </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'裁判文书'});" target="_blank" href="https://www.qcc.com/web/bigsearch/judgement" class="index-icon-text">
-                                        <div class="nav-icon-hot"></div> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/4179ee6ea9f44090a1d2967e23d68728.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>裁判文书</span>
-                                    </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'风控管家'});" target="_blank" href="https://r.qcc.com/monitor/overview" class="index-icon-text">
-                                        <div class="nav-icon-new"></div> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/de31618b137c4b11b5b8217bc53918cf.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>风控管家</span>
-                                    </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'客找找'});" target="_blank" href="https://z.qcc.com/qcc/kzz/transit" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/946a06148ef345ff9d98d54aab8b957e.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>客找找</span> </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'查商标'});" target="_blank" href="https://www.qcc.com/web/bigsearch/assets" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/0f9dbcb77e8347198861947d0ea181b9.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>查商标</span> </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'查老赖'});" target="_blank" href="https://www.qcc.com/more_deadbeat" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/19ad3c5cf6784badbc6b6a8fbf602faf.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>查老赖</span> </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'批量查企业'});" target="_blank" href="https://www.qcc.com/more_batchsearch" class="index-icon-text">
-                                        <div class="nav-icon-hot"></div> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/4210828f8e9d4559883cfc75a05cd774.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>批量查企业</span>
-                                    </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'舆情管家'});" target="_blank" href="https://y.qcc.com/monitor/monitor-list?from=nav" class="index-icon-text">
-                                        <div class="nav-icon-new"></div> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/80b366becfff41d180dc1fa64fabb89a.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>舆情管家</span>
-                                    </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'产业链'});" target="_blank" href="https://www.qcc.com/web/chain/search" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/a5509c6227a8447b9c3e096924de09d7.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>产业链</span> </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'千寻地图'});" target="_blank" href="https://www.qcc.com/map" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/a552b343e4c14f74886a5f0103d4ffcc.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>千寻地图</span> </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'全球企业'});" target="_blank" href="https://www.qcc.com/more_overseas" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/771ce45a9b004c689ee2b98b8a82dc9f.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>全球企业</span> </a> <a onclick="zhugeTrack('主页应用点击',{'应用名称':'接口API'});" target="_blank" href="https://openapi.qcc.com/data?source=websiteSwitch" class="index-icon-text"> <span class="m-b-xs block"> <i style="background-image: url(https://co-image.qichacha.com/boss/upload/8ba440a549bc4d5f909464d833bdd506.png?x-oss-process=image/resize,h_80);" class="nicon-yq"></i> </span> <span>接口API</span> </a> </div>
-                            </div>
-                            <div class="wrap-next" style="display: block;"></div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
