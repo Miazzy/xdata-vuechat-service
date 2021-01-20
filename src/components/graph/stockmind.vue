69f75cd56bf3458b350f6f4d3462ae094c4598f3
yunwisdom <qq695926920@163.com>
2021-01-20 09:26:51 +0800

    init

--- src/components/graph/stockmind.vue
+++ src/components/graph/stockmind.vue
@@ -14,6 +14,17 @@
         nodes: [],
         connections: [],
       }
+    },
+    methods: {
+        async setGraphData() {
+            this.nodes = await Betools.query.queryTableDataByWhereSQL('bs_company_flow_base' , `_where=(level,lt,100)~and(level,gt,0)~and(id,gt,17)&_sort=-id`); //获取所有公司信息
+            this.links = await Betools.query.queryTableDataByWhereSQL('bs_company_flow_link' , '_where=(status,in,0)&_sort=-id'); //获取所有公司关联信息
+            this.nodes.forEach(item => { item.text = item.name });
+            this.links.forEach(item => { item.source = item.from_company; item.target = item.to_company; });
+        },
+        onNodeExpand(node, e) {
+        
+        }
     }
   }
 </script>
