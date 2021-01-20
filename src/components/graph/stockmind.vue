baf7b1cb986105503e650c81638f3b6c60cf4411
yunwisdom <qq695926920@163.com>
2021-01-20 10:13:35 +0800

    init

--- src/components/graph/stockmind.vue
+++ src/components/graph/stockmind.vue
@@ -9,27 +9,51 @@ export default {
   components: { mindmap },
   data: () => ({
     data: [{
-      "name":"如何学习D3",
+      "name":"",
       "children": [
         {
-          "name":"预备知识",
-          "children": [
-            { "name":"HTML & CSS" },
-            { "name":"JavaScript" },
-          ]
+          "name":"股东",
+          "children": [],
         },
         {
-          "name":"安装",
-          "children": [
-            { "name": "折叠节点" }
-          ]
+          "name":"高管",
+          "children": [],
         },
         {
-          "name":"进阶",
-          "left": true
+          "name":"对外投资",
+          "children": [],
         },
-      ]
+        {
+          "name":"分支机构",
+          "children": [],
+        },
+      ],
     }]
-  })
+  }),
+  async created() {
+
+  },
+  async mounted() {
+    await this.setGraphData()
+  },
+  methods: {
+    async setGraphData() {
+        const companyID = 25; //获取所有公司信息
+        const nodes = await Betools.query.queryTableDataByWhereSQL('bs_company_flow_base' , `_where=(id,eq,25)~and(status,eq,0)&_sort=-id`);
+        const invs = await Betools.query.queryTableDataByWhereSQL('bs_company_stockholder' , `_where=(company_id,eq,25)~and(status,eq,valid)&_sort=-id`);
+        const persons = await Betools.query.queryTableDataByWhereSQL('bs_company_senior_executive' , `_where=(company_id,eq,25)~and(status,eq,valid)&_sort=-id`);
+        const branchs = await Betools.query.queryTableDataByWhereSQL('bs_company_branch' , `_where=(company_id,eq,25)&_sort=-id`);
+        const asInvs = await Betools.query.queryTableDataByWhereSQL('bs_company_investments' , `_where=(relation_company_id,eq,25)&_sort=-id`);
+        invs.map(item => {item.id = 'invs_' + item.id ; item.name = item.holder_name; item.desc = '控股: ' + item.ratio + '%'; });
+        persons.map(item => {item.id = 'persons_' + item.id ;item.name = item.holder_name; item.desc = item.position; });
+        branchs.map(item => {item.id = 'branchs_' + item.id ; item.desc = '法人: ' + item.principal; });
+        asInvs.map(item => {item.id = 'asInvs_' + item.id ;item.name = item.company_name; item.desc= '法人: ' + item.legal_person; });
+        this.data[0].name = nodes && nodes.length > 0 ? nodes[0].name : '' ,
+        this.data[0].children[0].children = invs;
+        this.data[0].children[1].children = persons;
+        this.data[0].children[2].children = asInvs;
+        this.data[0].children[3].children = branchs;
+    }
+  }
 }
 </script>
