db2d4795d3805e759f9b28c4cb9b436312a932c1
yunwisdom <qq695926920@163.com>
2021-01-20 17:14:04 +0800

    查询界面

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     build: {
         env: require('./prod.env'),
-        version: 'v1.3.42',
+        version: 'v1.3.43',
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
