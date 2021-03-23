3a5a7f717eb6abc7a0b3ca15f62b08e729647c6a
yunwisdom <qq695926920@163.com>
2021-03-23 12:03:12 +0800

    init

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     build: {
         env: require('./prod.env'),
-        version: 'v1.4.01',
+        version: 'v1.4.02',
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
