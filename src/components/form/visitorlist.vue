423b974828433e6dc7620d2ed783c750bb0e42e1
yunwisdom <qq695926920@163.com>
2021-03-23 12:00:03 +0800

    init

--- src/components/form/visitorlist.vue
+++ src/components/form/visitorlist.vue
@@ -303,12 +303,12 @@ export default {
               await vant.Dialog.confirm({
                 title: '到访确认',
                 message: `您好，被访人${element.create_by}尚未进行预约确认，您确定进行${element.visitor_name}的到访确认操作？`,
-              })
+              });
             } else {
               await vant.Dialog.confirm({
                 title: '到访确认',
                 message: `您好，请确认进行${element.visitor_name}的到访确认操作？`,
-              })
+              });
             }
 
             console.log(`key:`, key, ` value:`, value, ` element:`, element);
