a48296140709cd9329ce0fdcc1f5ae93940e9dae
yunwisdom <qq695926920@163.com>
2021-03-24 15:27:54 +0800

    init

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -671,13 +671,13 @@ export default {
             }
         },
         // 查询首页图片
-        async queryImagesUrl() {
+        async queryImagesUrl(type = 'APP') {
             try {
                 const userinfo = await Betools.storage.getStore('system_userinfo'); //获取当前登录用户信息
                 let whereSQL = null; //查询SQL
                 let images = await Betools.storage.getStore('system_app_image'); // 获取缓存中的图片
                 if (!images) { // 如果存在图片数据，则直接使用图片数据
-                    whereSQL = userinfo && userinfo.userid == 9058 ? '~and(create_by,eq,zhaoziyu)~and(bpm_status,in,4,5)~and(type,eq,APP)' : `~and(bpm_status,in,4,5)~and(create_by,in,admin,manager)~and(type,eq,APP)`;
+                    whereSQL = userinfo && userinfo.userid == 9058 ? `~and(create_by,eq,zhaoziyu)~and(bpm_status,in,4,5)~and(type,eq,${type})` : `~and(bpm_status,in,4,5)~and(create_by,in,admin,manager)~and(type,eq,${type})`;
                     images = await Betools.query.queryTableDataByWhereSQL('bs_home_pictures', `_where=(status,in,3)${whereSQL}&_fields=files&_sort=-id`);
                     images.map(item => {
                         item.files = `https://upload.yunwisdom.club:30443/${item.files}`;
