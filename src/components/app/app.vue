b918bf09847097c02d0c527125eb6cd52c6f80b1
yunwisdom <qq695926920@163.com>
2021-03-25 08:49:37 +0800

    init

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -679,9 +679,7 @@ export default {
                 if (!images) { // 如果存在图片数据，则直接使用图片数据
                     whereSQL = userinfo && userinfo.userid == 9058 ? `~and(create_by,eq,zhaoziyu)~and(bpm_status,in,4,5)~and(type,eq,${type})` : `~and(bpm_status,in,4,5)~and(create_by,in,admin,manager)~and(type,eq,${type})`;
                     images = await Betools.query.queryTableDataByWhereSQL('bs_home_pictures', `_where=(status,in,3)${whereSQL}&_fields=files&_sort=-id`);
-                    images.map(item => {
-                        item.files = `https://upload.yunwisdom.club:30443/${item.files}`;
-                    });
+                    images.map(item => { item.files = `https://upload.yunwisdom.club:30443/${item.files}`;  });
                     Betools.storage.setStore('system_app_image', JSON.stringify(images), 3600 * 24 * 3);
                 }
                 return images;
