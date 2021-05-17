2192a7bae2b1ee1ae87df9fc59c631f95a0fdf48
yunwisdom <qq695926920@163.com>
2021-05-17 10:46:17 +0800

    init

--- src/components/form/goodslist.vue
+++ src/components/form/goodslist.vue
@@ -108,8 +108,6 @@ export default {
               { text: '刷新', value: 2 , icon: 'replay' },
               { text: '搜索', value: 3 , icon: 'search' },
               { text: '重置', value: 4 , icon: 'aim' },
-              { text: '应用', value: 5 , icon: 'apps-o' },
-              { text: '首页', value: 6 , icon: 'wap-home-o' },
             ],
             isLoading: false,
             loading: false,
